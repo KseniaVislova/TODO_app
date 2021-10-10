979230c801f61cde00d7b544a0cbc5cabad127b7
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2021-10-10 20:43:54 +0300

    Initial dist subtree commit

---
+++ precache-manifest.fabeed37dc004ab3d9330bbf2a111fb7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1000d7308dbb3dad87e",
+    "url": "/TODO_app/css/app.14e1e1e1.css"
+  },
+  {
+    "revision": "9f309eb54a643f251e8c",
+    "url": "/TODO_app/css/chunk-4cbf8f0a.7b3984e2.css"
+  },
+  {
+    "revision": "66d1de7da4c0139d66e4",
+    "url": "/TODO_app/css/chunk-719ff7a0.f5c719b5.css"
+  },
+  {
+    "revision": "99fd98916dfdd339aebe",
+    "url": "/TODO_app/css/chunk-7fe18359.0f028bec.css"
+  },
+  {
+    "revision": "6d5452bee7eb9a783531",
+    "url": "/TODO_app/css/chunk-c33ddb9a.4a78763f.css"
+  },
+  {
+    "revision": "6d7d60413ab1b1047329d314fe131f50",
+    "url": "/TODO_app/index.html"
+  },
+  {
+    "revision": "d1000d7308dbb3dad87e",
+    "url": "/TODO_app/js/app.36ee4caf.js"
+  },
+  {
+    "revision": "cb818b82d490769e12e0",
+    "url": "/TODO_app/js/chunk-3f4db572.69af9ffd.js"
+  },
+  {
+    "revision": "9f309eb54a643f251e8c",
+    "url": "/TODO_app/js/chunk-4cbf8f0a.56b52740.js"
+  },
+  {
+    "revision": "66d1de7da4c0139d66e4",
+    "url": "/TODO_app/js/chunk-719ff7a0.5cbf2c81.js"
+  },
+  {
+    "revision": "99fd98916dfdd339aebe",
+    "url": "/TODO_app/js/chunk-7fe18359.32c94240.js"
+  },
+  {
+    "revision": "6d5452bee7eb9a783531",
+    "url": "/TODO_app/js/chunk-c33ddb9a.190ac4fe.js"
+  },
+  {
+    "revision": "e18c06e048de49fc324e",
+    "url": "/TODO_app/js/chunk-vendors.45f429c3.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/TODO_app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TODO_app/robots.txt"
+  }
+]);
